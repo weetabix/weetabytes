17a8972648c7a882b1836eb60f48b1aa7d054c1e
weetabix <weetabix@weetabix.net>
2016-10-09 18:41:02 -0600

    Made some changes to the display to reflect the eventual options for adding tasks.

--- todoist-cli/tsel.py
+++ todoist-cli/tsel.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import todoist
 import json
 from pprint import pprint
@@ -7,12 +8,13 @@ results=api.sync(commands=[])
 
 # Header
 
-print(' {0:^3} ┊{2:<3}┊ {1:^40} {3:^}'.format("#","Task Notes","Pri","Project"))
+print(' {0:^3} ┊{2:<3}┊ {1:^40} {3:^}'.format("","Task Notes","⬆@@","Proje##"))
 print(' {:┉^3} ┊{:┉<3}┊ {:┉^40} {:┉^7}'.format("","","",""))
 
 for i in range(0,len(results['items'])):
     if results['items'][i]['item_order'] != 14:
-        prio = "▬" * (results['items'][i]['priority'] - 1)
+        prio = "➡" * (results['items'][i]['priority'] - 1)
+#        prio = "▬" * (results['items'][i]['priority'] - 1)
         for j in range(0,len(results['projects'])):
             if results['items'][i]['project_id'] == results['projects'][j]['id']:
                 proj = results['projects'][j]['name']
